e6503634947d4ca813297b3a816dc4d43165b87b
Ronis [dev] <admin@ronis.de>
2013-05-07 12:15:02 +0200

    add multi site support

--- urls.py
+++ urls.py
@@ -1,19 +1,21 @@
 # -*- coding: utf-8 -*
 from django.conf.urls import patterns
-# from django.conf.urls import include
 from django.conf.urls import url
 
 urlpatterns = patterns('cms.views',
-	#	news/id100/
+	# News
+	# news/id100/
 	url(r'^(?P<url>\w+)/id(?P<id>\d+)/$', 'news_detail', name='cms_news_detail'),
-	#	news/2000/
+	# news/2000/
 	url(r'^(?P<url>[-\w]+)/(?P<year>\d{4})/$', 'news_year_archive', name='cms_news_year_archive'),
-	#	news/2000/04/
+	# news/2000/04/
 	url(r'^(?P<url>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/$', 'news_month_archive', name='cms_news_month_archive'),
-	#	news/2000/04/30/
+	# news/2000/04/30/
 	url(r'^(?P<url>[-\w]+)/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$', 'news_day_archive', name='cms_news_day_archive'),
-	#	article/3/	-	article page 3
+
+	# Article
+	# article/3/ article page 3
 	url(r'^(?P<url>[-\D\w/_]+)/(?P<page>\d+)/$', 'category', name='cms_category_page'),
-	#	article/
+	# article/
 	url(r'^(?P<url>[-\D\w/_]+)/$', 'category', name='cms_category'),
 )
